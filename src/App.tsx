ce14d31d7ed135e6078319ad3c6a78b53caf2d6c
Nikesh Joshi <nikesh.joshi@office.mypay.com.np>
2025-07-31 09:39:30 +0545

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ function App() {
 
         setStatus('Sending...');
         try {
+            // await axios.post('http://localhost:5000/send.', contactForm);
             await axios.post('https://client-hexa-1.onrender.com/send', contactForm);
 
             setStatus("✅ Message sent successfully!");
